90b243b2a23e5f20911aff4223285def8d399ec0
mshehroze-dev <m.shehroze@nuvra.agency>
2026-03-03 12:03:35 +0500

    Add supabase/functions/hello/index.ts

---
+++ supabase/functions/hello/index.ts
@@ -0,0 +1,32 @@
+
+// Follow this setup guide to integrate the Deno language server with your editor:
+// https://deno.land/manual/getting_started/setup_your_environment
+// This enables autocomplete, go to definition, etc.
+
+import { serve } from "https://deno.land/std@0.168.0/http/server.ts"
+
+console.log("Hello from Functions!")
+
+serve(async (req) => {
+  const { name } = await req.json()
+  const data = {
+    message: `Hello ${name}!`,
+  }
+
+  return new Response(
+    JSON.stringify(data),
+    { headers: { "Content-Type": "application/json" } },
+  )
+})
+
+/* To invoke locally:
+
+  1. Run `supabase start` (see: https://supabase.com/docs/reference/cli/supabase-start)
+  2. Make an HTTP request:
+
+  curl -i --location --request POST 'http://127.0.0.1:54321/functions/v1/hello' \
+    --header 'Authorization: Bearer [YOUR_ANON_KEY]' \
+    --header 'Content-Type: application/json' \
+    --data '{"name":"Functions"}'
+
+*/
