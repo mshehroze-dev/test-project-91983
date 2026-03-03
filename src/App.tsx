21d3015b5f385ed82829870b2e37188a9081abe6
mshehroze-dev <m.shehroze@nuvra.agency>
2026-03-03 12:03:12 +0500

    Add src/App.tsx

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+import { Routes, Route } from "react-router-dom";import { Layout } from "./components/layout/Layout";
+import Home from "./pages/Home";import Contact from "./pages/Contact";
+export default function App() {
+  return (    <Layout>
+      <Routes>
+        <Route path="/" element={<Home />} />          <Route path="/contact" element={<Contact />} />
+      </Routes>
+    </Layout>  );
+}
